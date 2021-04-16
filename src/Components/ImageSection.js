a911c93b1f1bb0a9b2d03ff04c7c90b31f921358
prosolver-git <17pt36@gapps.psgtech.ac.in>
2021-04-17 03:11:37 +0530

    commit

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -30,7 +30,7 @@ function ImageSection() {
                         <p>: +91 99949 55796</p>
                     </div>
                 </div>
-                <button className="btn" href="https://drive.google.com/file/d/1XtL8KoNwKeExDNzIvj0XAiOJ2KPTWSL0/view?usp=sharing">Download Cv</button>
+                <button className="btn"><a href="https://drive.google.com/file/d/1XtL8KoNwKeExDNzIvj0XAiOJ2KPTWSL0/view?usp=sharing"></a>Download Cv</button>
             </div>
         </div>
     )
