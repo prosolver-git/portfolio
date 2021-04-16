1cb6e9187bf3656612efaf4891b92f006f9db5e2
prosolver-git <17pt36@gapps.psgtech.ac.in>
2021-04-17 04:49:35 +0530

    change

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -30,7 +30,7 @@ function ImageSection() {
                         <p>: +91 99949 55796</p>
                     </div>
                 </div>
-                <a className="btn" href="https://drive.google.com/file/d/1XtL8KoNwKeExDNzIvj0XAiOJ2KPTWSL0/view?usp=sharing">Download Cv</a>
+                <a className="btn" href="https://drive.google.com/file/d/1XtL8KoNwKeExDNzIvj0XAiOJ2KPTWSL0/view?usp=sharing" target="_blank">Download Cv</a>
             </div>
         </div>
     )
